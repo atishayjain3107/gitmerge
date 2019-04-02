20e331dcdf07a7d5b91c10463aa3faaf64657c35
atishayjain3107 <atishayjain3107@gmail.com>
2019-04-02 23:54:40 +0530

    commit2

--- file.cpp
+++ file.cpp
@@ -5,6 +5,11 @@ int main()
 {
 	char op;
     	float num1, num2;
+	cout << "Enter operator either + or - or * or /: ";
+    	cin >> op;
+
+    	cout << "Enter two operands: ";
+    	cin >> num1 >> num2;
 	return(0);
 }
 
