ce3087948a2b017dc3b272d98db1c57dd11cadb1
atishayjain3107 <atishayjain3107@gmail.com>
2019-04-02 23:58:36 +0530

    commit1 to br1

--- file.cpp
+++ file.cpp
@@ -10,7 +10,15 @@ int main()
 
     	cout << "Enter two operands: ";
     	cin >> num1 >> num2;
+	switch(op)
+	{
+        	case '+':
+            	cout << num1+num2;
+            	break;
+
+	}
 	return(0);
+
 }
 
 
